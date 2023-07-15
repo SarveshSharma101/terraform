857915aa205062bd4c191e202c31ad44fe4851c8
SarveshSharma101 <ss541271@gmail.com>
2023-07-15 20:02:29 +0530

    Added implementation to provision resource policay for snapshot and creating a s3 bucket

--- GCP/VM-Disk/main.tf
+++ GCP/VM-Disk/main.tf
@@ -13,6 +13,56 @@ provider "google" {
 	project = "terraform-practice-392512"
 }
 
+resource "google_compute_resource_policy" "disk-snapshot-1" {
+	name = "disk-snapshot-1"
+	region = "asia-south1"
+	snapshot_schedule_policy {
+		schedule {
+			daily_schedule {
+				days_in_cycle = 1
+				start_time = "06:00"
+			}
+		}
+
+		retention_policy {
+			max_retention_days = 10
+			on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+		}
+
+		snapshot_properties {
+			labels = {
+				disk = "disk-snapshot-1"
+				storage_locations = "asia"
+			}
+		}	
+	}
+}
+
+resource "google_compute_resource_policy" "disk-snapshot-2" {
+	name = "disk-snapshot-2"
+	region = "asia-south1"
+	snapshot_schedule_policy {
+		schedule {
+			hourly_schedule {
+				hours_in_cycle = 6
+				start_time = "00:00"
+			}
+		}
+
+		retention_policy {
+			max_retention_days = 30
+			on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+		}
+
+		snapshot_properties {
+			labels = {
+				disk = "disk-snapshot-1"
+				storage_locations = "asia-south1"
+			}
+		}	
+	}
+}
+
 resource "google_compute_instance" "instace1" {
 	name = "t-ce-1"
 	machine_type = "e2-micro"
@@ -120,5 +170,35 @@ resource "google_compute_disk" "disk-t-ce-1" {
 	type = "pd-balanced"
 
 	size = 50
-	
+}
+
+resource "google_compute_disk_resource_policy_attachment" "attachment1" {
+	name = google_compute_resource_policy.disk-snapshot-1.name
+	disk = google_compute_disk.disk-t-ce-1.name
+	zone = google_compute_disk.disk-t-ce-1.zone
+}
+
+resource "google_compute_region_disk_resource_policy_attachment" "attachment2" {
+	name = google_compute_resource_policy.disk-snapshot-2.name
+	disk = google_compute_region_disk.disk-t-ce-2.name
+	region = "asia-south1"
+}
+
+
+resource "google_storage_bucket" "sotrage_bucket" {
+	name = "sotrage_bucket1"
+	location = "US"
+	storage_class = "STANDARD"
+	uniform_bucket_level_access = true
+	versioning {
+		enabled = true
+	}
+	force_destroy = true
+}
+
+resource "google_storage_bucket_object" "dummy" {
+	name = "dummy"
+	source = "dummy.txt"
+	content_type = "text/plain"
+	bucket = google_storage_bucket.sotrage_bucket.id
 }
